f6fd3d5e44ded3965eb662c3d2a1f4f658b6b54d
drashtichavda <143491700+drashtichavda@users.noreply.github.com>
2024-04-05 08:39:55 +0530

    Create App.js

---
+++ hooks/src/App.js
@@ -0,0 +1,14 @@
+import Refrence2 from "./Refrence2";
+
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Refrence /> */}
+    <Refrence2 />
+    </div>
+  );
+}
+
+export default App;
+
